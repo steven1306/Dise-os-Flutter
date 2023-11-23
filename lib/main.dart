b16441805d2910f2563ea1f7060e96b7f55f93a7
steven1306 <stev.valderrama@udla.edu.co>
2023-11-23 09:49:33 -0500

    proyecto terminado

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,26 @@
 import 'package:disenos/screens/basic_design.dart';
+import 'package:disenos/screens/home_screen.dart';
+import 'package:disenos/screens/scroll_design.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
-void main() => runApp(const MyApp());
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.light);
+
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Material App',
-      initialRoute: 'basic_design',
-      routes: {'basic_design': (_) => BasicDsesignScreen()},
+      theme: ThemeData.dark(),
+      initialRoute: 'home_screen',
+      routes: {
+        'basic_design': (_) => BasicDesignScreen(),
+        'scroll_screen': (_) => ScrollScreen(),
+        'home_screen': (_) => HomeScreen(),
+      },
     );
   }
 }
--- lib/screens/basic_design.dart
+++ lib/screens/basic_design.dart
@@ -1,13 +1,25 @@
 import 'package:flutter/material.dart';
 
-class BasicDsesignScreen extends StatelessWidget {
+class BasicDesignScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         body: Column(
       children: [
-        Image(image: AssetImage('assets/forest-landscape.jpg')),
-        Title()
+        // Imagen
+        Image(image: AssetImage('assets/landscape.jpg')),
+
+        // Title
+        Title(),
+
+        // Button Section
+        ButtonSection(),
+
+        // Description
+        Container(
+            margin: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+            child: Text(
+                'Laboris enim occaecat do et id velit dolore qui Lorem. Dolore ut incididunt consequat sunt do nisi aliqua pariatur ex quis cupidatat. Enim aliqua deserunt voluptate Lorem Lorem in cillum aute veniam. Ipsum irure eiusmod occaecat qui dolor consectetur aliquip deserunt sint labore qui aute mollit. Veniam duis adipisicing do mollit aliquip minim.'))
       ],
     ));
   }
@@ -15,8 +27,8 @@ class BasicDsesignScreen extends StatelessWidget {
 
 class Title extends StatelessWidget {
   const Title({
-    super.key,
-  });
+    Key? key,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -28,10 +40,10 @@ class Title extends StatelessWidget {
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               Text(
-                'oeschinen lake campground',
+                'Oeschinen Lake Campground',
                 style: TextStyle(fontWeight: FontWeight.bold),
               ),
-              Text('kandersteg,Switzerland',
+              Text('Kandersteg, Switzerland',
                   style: TextStyle(color: Colors.black45)),
             ],
           ),
@@ -40,6 +52,52 @@ class Title extends StatelessWidget {
           Text('41'),
         ],
       ),
+    );
+  }
+}
+
+class ButtonSection extends StatelessWidget {
+  const ButtonSection({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          CustomButton(icon: Icons.call, text: 'Call'),
+          CustomButton(icon: Icons.map_sharp, text: 'Route'),
+          CustomButton(icon: Icons.share, text: 'Share'),
+        ],
+      ),
+    );
+  }
+}
+
+class CustomButton extends StatelessWidget {
+  final IconData icon;
+  final String text;
+
+  const CustomButton({
+    Key? key,
+    required this.icon,
+    required this.text,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Icon(
+          this.icon,
+          color: Colors.blue,
+          size: 30,
+        ),
+        Text(this.text, style: TextStyle(color: Colors.blue))
+      ],
     );
   }
 }
